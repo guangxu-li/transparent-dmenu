3807633b444c5af3f6e7d0ebd79f858a14bc128f
Guangxu Li <guangxu.li@nyu.edu>
2021-04-19 16:23:15 -0400

    chore: clean up

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-static const unsigned int bgalpha = 0xFF;
+static const unsigned int bgalpha = 0xCC;
 //static const unsigned int fgalpha = OPAQUE;
 static const unsigned int fgalpha = 0xFF;
 /* -fn option overrides fonts[0]; default X11 font or font set */
